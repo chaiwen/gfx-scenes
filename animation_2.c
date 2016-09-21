f470ff66e1b649d0a333594bc9fd1c081d56828f
chaiwen <cc3636@columbia.edu>
2016-09-21 16:02:32 -0400

    wavy springy effect

--- animation_2.c
+++ animation_2.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	float dy = 1;
 	float radius = 0.03;
 	
-	int numSprings = 50;
+	int numSprings = 80;
 	int numParticles = numSprings * 2;
 
 	float px = 10.0;
@@ -27,19 +27,18 @@ int main(int argc, char **argv)
 	float vy = 0.0;
 
 	for (i = 0; i < numParticles; i++) {
-		x = dx * cos((i*360/numParticles) * PI/180);
-		y = dy * sin((i*360/numParticles) * PI/180);
+		x = dx * cos(i * 2 * PI / numParticles);
+		y = dy * sin(i * 2 * PI / numParticles);
 		printf("  <particle m=\"1.0\" px=\"%f\" py=\"%f\" ", x, y);
 		printf("vx=\"%f\" vy=\"%f\" fixed=\"0\" radius=\"%f\"/>\n", vy, vy, radius);
 	}
 
-	float kconst = 1.0;
+	float kconst = 0.8;
 	float lconst = 1.0;
 	for (i = 0; i < numSprings; i++) {
 		printf("  <edge i=\"%d\" j=\"%d\" radius=\"0.001\"/>", i, i + numSprings);
 
-		kconst *= 0.9;
-		lconst *= 0.8;
+		lconst = 0.2 + 0.2 * sin(3 * 2 * PI * i/numSprings);
 		printf("  <springforce edge=\"%d\" k=\"%f\" l0=\"%f\"/>", i, kconst, lconst);
 	}
 	for (i = 0; i < numParticles; i++) {
@@ -47,7 +46,7 @@ int main(int argc, char **argv)
         HSVtoRGB(&r, &g, &b, (i*360/numParticles), 0.9, 0.9);
 		
 		printf("  <particlecolor i=\"%d\" r=\"%f\" g=\"%f\" b=\"%f\"/>\n", i, r, g, b);
-		printf("  <particlepath i=\"%d\" duration=\"1.5\" r=\"%f\" g=\"%f\" b=\"%f\"/>\n", i, r, g, b);
+		printf("  <particlepath i=\"%d\" duration=\"0.5\" r=\"%f\" g=\"%f\" b=\"%f\"/>\n", i, r, g, b);
     }
 	for (i = 0; i < numSprings; i++) {
         float r,g,b;
@@ -58,7 +57,6 @@ int main(int argc, char **argv)
 		printf("  <edgecolor i=\"%d\" r=\"%f\" g=\"%f\" b=\"%f\"/>\n", i, r, g, b);
     }
 
-	//printf("\n  <simplegravity fx=\"0.0\" fy=\"-1.81\"/>\n");
 	printf("\n  <backgroundcolor r=\"1.0\" g=\"1.0\" b=\"1.0\"/>\n");
 	printf("</scene>\n");
 
